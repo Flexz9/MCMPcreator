2a535d4309d9f047f89a9974a67480ee7107b3e1
Thomas <thomas@demaerschalck.com>
2024-09-15 16:52:01 +0200

    Persistent

--- src/app/data/mod.service.ts
+++ src/app/data/mod.service.ts
@@ -5,9 +5,27 @@ import { Mod } from './mod.model';
   providedIn: 'root',
 })
 export class ModService {
+  private modLoader: string;
+  private version: string;
   private modList: Mod[] = [];
 
-  constructor() {}
+  constructor() {
+    const savedModLoader = localStorage.getItem('modLoader');
+    const savedVersion = localStorage.getItem('version');
+    const savedModList = localStorage.getItem('modList');
+
+    this.modLoader = savedModLoader || 'neoForge';
+    this.version = savedVersion || '1.20.1';
+
+    if (savedModList) {
+      try {
+        this.modList = JSON.parse(savedModList);
+      } catch (e) {
+        console.error('Error parsing mod list from localStorage', e);
+        this.modList = [];
+      }
+    }
+  }
 
   getmodList(): Mod[] {
     return this.modList;
@@ -31,9 +49,33 @@ export class ModService {
     }
 
     console.log(this.modList);
+    this.saveModList();
   }
 
   remove(slug: string): void {
     this.modList = this.modList.filter(mod => mod.slug !== slug);
+    this.saveModList();
+  }
+
+  setVersion(version: string) {
+    this.version = version;
+    localStorage.setItem('version', this.version);
+  }
+
+  setModLoader(modLoader: string) {
+    this.modLoader = modLoader;
+    localStorage.setItem('modLoader', this.modLoader);
+  }
+
+  getVersion(): string {
+    return this.version;
+  }
+
+  getModLoader(): string {
+    return this.modLoader;
+  }
+  
+  private saveModList(): void {
+    localStorage.setItem('modList', JSON.stringify(this.modList));
   }
 }
